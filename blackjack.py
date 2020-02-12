3e07a870d6e09a2470e0f8eb0473c8692461379c
Hjalmar <hjalmar.fjallborg@gmail.com>
2020-02-12 23:05:53 +0100

    Minor tweaks and cleanup

--- blackjack.py
+++ blackjack.py
@@ -1,22 +1,8 @@
+
 from itertools import product
 from random import shuffle, choice
 import pygame
 import os
-
-
-
-face_dict = {
-	'S' : 'Spades',
-	'H' : 'Hearts',
-	'C' : 'Clubs',
-	'D' : 'Diamonds'
-}
-
-suit_dict = {
-	'2' : 'Two',
-	'3' : 'Three',
-	'4' : 'Four'
-}
 
 
 def get_score(hand):
@@ -34,8 +20,6 @@ def get_score(hand):
 		return score
 
 
-
-
 class Game():
 
 	suits = ['S', 'H', 'C', 'D']
@@ -43,7 +27,6 @@ class Game():
 
 	def random_deck(cls):
 		"""Generate a shuffled deck of cards.
-
 		
 		Creates a shuffled list of tuples consisting of a suit (e.g.
 		hearts) and a face (e.g. ace).
@@ -61,42 +44,24 @@ class Game():
 	
 	def play(self):
 		"""Start a game sequence."""
-
+		
 		self.deck = self.random_deck()
 
 		# Deal cards
 		your_hand = [self.deck.pop(), self.deck.pop()]
 		dealers_hand = [self.deck.pop(), self.deck.pop()]
+		
 		while True:
 			score = get_score(your_hand)
 			dealer_score = get_score(dealers_hand)
 
-			#for i in range(len(your_hand)):
-			#	if your_hand[i][0] == 'J' or your_hand[i][0] == 'Q' or your_hand[i][0] == 'K':
-			#		score += 10
-			#	elif your_hand[i][0] == 'A':
-			#		if score <= 10:
-			#			score += 11
-			#		else:
-			#			score += 1
-			#	else:
-			#		score += int(your_hand[i][0])
-
-			#for i in range(len(dealers_hand)):
-			#	if dealers_hand[i][0] == 'J' or dealers_hand[i][0] == 'Q' or dealers_hand[i][0] == 'K':
-			#		dealer_score += 10
-			#	elif dealers_hand[i][0] == 'A':
-			#		if dealer_score <= 10:
-			#			dealer_score += 11
-			#		else:
-			#			dealer_score += 1
-			#	else:
-			#		dealer_score += int(dealers_hand[i][0])
-
 			print(f'Dealers hand: {dealers_hand}. Score: {dealer_score} \n')
 			print(f'Your hand: {your_hand}. Score: {score}\n')
 			
-			action = input('Hit or Stand? [h/s]: ')
+			if score == 21:
+				action = 's'
+			else:
+				action = input('Hit or Stand? [h/s]: ')
 			if action == 's':
 
 				while True:
@@ -128,7 +93,6 @@ class Game():
 								print('You win!')
 							break
 
-
 				break
 			else:
 				your_hand.append(self.deck.pop())
@@ -138,9 +102,6 @@ class Game():
 					break
 
 
-
-
-
-a = Game()
-a.play()
-
+if __name__ == '__main__':
+	a = Game()
+	a.play()
